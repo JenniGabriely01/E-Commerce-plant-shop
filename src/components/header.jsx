197af46482e4be5d4deaacb7db640aab376cc3e6
Jennifer <lopesdossantosjennifergabriely@gmail.com>
2024-04-14 02:38:07 -0300

    Iniciando projeto e criando o componente header

---
+++ src/components/header.jsx
@@ -0,0 +1,43 @@
+import logo from '../assets/logo.svg'
+import search from '../assets/search.svg'
+import cart from '../assets/cart.svg'
+import person from '../assets/person.svg'
+import { Link } from 'react-router-dom';
+
+export function Header() {
+    return (
+        <header className="flex items-center justify-between w-full py-11 px-24 header-container">
+            <div className="flex">
+                <img className="mr-12" src={logo} alt="logo" />
+
+                <ul className="flex">
+                    <li className="mr-12 text-[#8F8F8F]">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="mr-12 text-[#8F8F8F]">
+                        <Link>Products</Link>
+                    </li>
+                    <li className="mr-12 text-[#8F8F8F]">
+                        <Link>Contacts</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="flex items-center">
+
+                <div className="relative mr-12">
+                    <input type="text" className="border border-gray-300 rounded-xl px-4 py-2 w-80 placeholder-gray-700" placeholder="What are you looking for?" />
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+                        <img src={search} alt="search" />
+                    </span>
+                </div>
+
+                <img src={cart} alt="icon cart" className="mr-12" />
+                <Link>
+                    <img src={person} alt="icon person" />
+                </Link>
+            </div>
+
+        </header>
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,5 @@
+export function Home(){
+    return(
+        <h3>oioioi</h3>
+    )
+}
